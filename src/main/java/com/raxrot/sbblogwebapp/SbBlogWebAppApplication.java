cbeff9e25e2cba6dcff9d4e6b23977ef2cd20c54
Vlad Bulahov <dasistperfektosss@gmail.com>
2024-12-30 15:22:11 +0000

    init

---
+++ src/main/java/com/raxrot/sbblogwebapp/SbBlogWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.raxrot.sbblogwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBlogWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbBlogWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raxrot/sbblogwebapp/SbBlogWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raxrot.sbblogwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbBlogWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
