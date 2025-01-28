7617cc499a67ceded8182d2c082a3a3a64856f0f
dashscode <m4daria@gmail.com>
2025-01-28 19:43:08 +0100

    Create PostMapper

---
+++ src/main/java/com/raxrot/sbblogwebapp/mapper/PostMapper.java
@@ -0,0 +1,35 @@
+package com.raxrot.sbblogwebapp.mapper;
+
+import com.raxrot.sbblogwebapp.dto.PostDto;
+import com.raxrot.sbblogwebapp.entity.Post;
+
+public class PostMapper {
+
+    //map Post entity to PostDTO
+    public PostDto mapToPostDto(Post post) {
+        PostDto postDto=PostDto.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .url(post.getUrl())
+                .content(post.getContent())
+                .shortDescription(post.getShortDescription())
+                .createdOn(post.getCreatedOn())
+                .updatedOn(post.getUpdatedOn())
+                .build();
+        return postDto;
+    }
+
+    //map PostDTO entity to Post
+    public Post mapToPost(PostDto postDto) {
+        Post post=Post.builder()
+                .id(postDto.getId())
+                .title(postDto.getTitle())
+                .url(postDto.getUrl())
+                .content(postDto.getContent())
+                .shortDescription(postDto.getShortDescription())
+                .createdOn(postDto.getCreatedOn())
+                .updatedOn(postDto.getUpdatedOn())
+                .build();
+        return post;
+    }
+}
